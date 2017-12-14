97ee04543a3b2eddd8d1cfe660d59460bad6512d
Vinge1718 <vinge1718@gmail.com>
2017-12-14 16:40:51 +0300

    edit database details

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,30 @@
 import org.sql2o.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DB {
+    private static URI dbUri;
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do","v", "6442");
+
+    static {
+        Logger logger = LoggerFactory.getLogger(DB.class);
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/to_do");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "v" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "6442" : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            logger.error("Unable to connect to database.");
+        }
+    }
 }
