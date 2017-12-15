a967a896d750c2610fcebe0baebe792f6c739ab1
Vinge1718 <vinge1718@gmail.com>
2017-12-15 16:30:39 +0300

    edit the data base details

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 
 public class DB {
     private static URI dbUri;
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do","v", "6442");
+    public static Sql2o sql2o;
 
     static {
         Logger logger = LoggerFactory.getLogger(DB.class);
